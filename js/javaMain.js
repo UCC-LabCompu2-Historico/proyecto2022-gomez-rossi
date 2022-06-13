c53eac879f938e4e965fdd3cd9f7c50f63fc80e0
giulisrossi <giuliana.rossi.da@gmail.com>
2022-06-13 14:34:40 -0300

    Añadimos Canvas

--- js/javaMain.js
+++ js/javaMain.js
@@ -1,96 +1,95 @@
 const contenedor = document.getElementById("test");
-const botonRes = document.getElementById("boton");
+const botonRes = document.getElementById("botonAbajo");
 const resultadoTest = document.getElementById("resultado");
 
 const preguntas = [
     {
         pregunta: "1. ¿Cuál es el lenguaje que se usa para crear páginas web?",
         respuestas: {
-            a: "JAVA",
-            b: "HTML-5",
-            c: "PERL",
+            a: "JAVA <br>",
+            b: "HTML-5 <br>",
+            c: "PERL <br>",
         },
         respuestaCorrecta: "b",
     },
     {
         pregunta: "2. ¿Qué es un lenguaje de programación?",
         respuestas: {
-            a: "Lenguaje que puede ser utilizado para controlar el comportamiento de algo.",
-            b: "Lenguaje que se utiliza para interactuar con los usuarios",
-            c: "Lenguaje artificial que puede ser utilizado para controlar el comportamiento de una máquina.",
+            a: "Lenguaje que puede ser utilizado para controlar el comportamiento de algo. <br>",
+            b: "Lenguaje que se utiliza para interactuar con los usuarios. <br>",
+            c: "Lenguaje artificial que puede ser utilizado para controlar el comportamiento de una máquina. <br>",
         },
         respuestaCorrecta: "c",
     },
     {
         pregunta: "3. ¿A qué se le llama pseudocódigo?",
         respuestas: {
-            a: "x",
-            b: "x ",
-            c: "x ",
-            d: "Es una serie de normas léxicas y gramaticales parecidas a la mayotía de los lenguajes de programación.",
+            a: "Es un lenguaje de programación. <br>",
+            b: "No es un término utilizado en programación. <br>",
+            c: "Es una serie de normas léxicas y gramaticales parecidas a la mayoría de los lenguajes de programación. <br>",
         },
-        respuestaCorrecta: "d",
+        respuestaCorrecta: "c",
     },
     {
         pregunta: "4. ¿Cuales son los lenguajes de programación más comunes?",
         respuestas: {
-            a: "C, C++, PASCAL",
-            b: "JAVA, JavaScript, HTML-5 ",
-            c: "PYTHON, PERL, JavaScript ",
+            a: "C, C++, PASCAL <br>",
+            b: "JAVA, JavaScript, HTML-5 <br>",
+            c: "PYTHON, PERL, JavaScript <br>",
         },
         respuestaCorrecta: "a",
     },
     {
         pregunta: "5. ¿Diferencia entre Alto Nivel y Bajo Nivel?",
         respuestas: {
-            a: "Bajo Nivel: permiten al programador olvidarse por completo del funcionamiento interno de la máquina. Alto Nivel: prácticamente diseñados a medida del hardware.",
-            b: "Bajo Nivel: lenguaje independiente de la arquitectura del ordenador. Alto Nivel: lenguaje de máquina.",
-            c: "Bajo Nivel: totalmente dependiente de la máquina. Alto Nivel: son aquellos que se encuentran más cercanos al lenguaje natural que al lenguaje de máquina.",
+            a: "Bajo Nivel: permiten al programador olvidarse por completo del funcionamiento interno de la máquina. Alto Nivel: prácticamente diseñados a medida del hardware. <br>",
+            b: "Bajo Nivel: lenguaje independiente de la arquitectura del ordenador. Alto Nivel: lenguaje de máquina. <br>",
+            c: "Bajo Nivel: totalmente dependiente de la máquina. Alto Nivel: son aquellos que se encuentran más cercanos al lenguaje natural que al lenguaje de máquina. <br>",
         },
         respuestaCorrecta: "c",
     },
     {
         pregunta: "6. ¿Qué es un Algoritmo?",
         respuestas: {
-            a: "Conjunto desordenado de operaciones para encontrar la solución a un problema.",
-            b: "Conjunto de sentencias para resolver un problema.",
-            c: "Conjunto ordenado y finito de operaciones que permiten hallar la solución de un problema. ",
+            a: "Conjunto desordenado de operaciones para encontrar la solución a un problema. <br>",
+            b: "Conjunto de sentencias para resolver un problema. <br>",
+            c: "Conjunto ordenado y finito de operaciones que permiten hallar la solución de un problema. <br>",
         },
         respuestaCorrecta: "c",
     },
     {
         pregunta: "7. ¿Qué significa CSS?",
         respuestas: {
-            a: "x",
-            b: "Cascading Style Sheets ",
-            c: "x",
+            a: "Colorful Style Sheets. <br>",
+            b: "Cascading Style Sheets. <br> ",
+            c: "Computer Style Sheets. <br>",
         },
         respuestaCorrecta: "b",
     },
     {
         pregunta: "8. ¿Por qué es tan conocido JAVA?",
         respuestas: {
-            a: "Porque es simple.",
-            b: "Porque tiene una filosofía única: 'se escribe una vez y ejecuta en cualquier lugar'. ",
-            c: "Porque tiene pocos tipos de datos y pocas clases en sus paquetes.",
+            a: "Porque es simple. <br>",
+            b: "Porque tiene una filosofía única: 'se escribe una vez y ejecuta en cualquier lugar'. <br>",
+            c: "Porque tiene pocos tipos de datos y pocas clases en sus paquetes. <br>",
         },
         respuestaCorrecta: "b",
     },
     {
         pregunta: "9. ¿Para qué se utiliza JavaScript?",
         respuestas: {
-            a: "Es el lenguaje de programación que debes usar para añadir características interactivas a tu sitio web. ",
-            b: "Es el lenguaje de programación que debes usar para añadir estilo a tu sitio web.",
-            c: "Lenguaje para escribir un código de una página web.",
+            a: "Es el lenguaje de programación que debes usar para añadir características interactivas a tu sitio web. <br>",
+            b: "Es el lenguaje de programación que debes usar para añadir estilo a tu sitio web. <br>",
+            c: "Lenguaje para escribir un código de una página web. <br>",
         },
         respuestaCorrecta: "a",
     },
     {
         pregunta: "10. ¿Qué tipo de lenguaje es Python?",
         respuestas: {
-            a: "Es un lenguaje difícil de escribir y comprender.",
-            b: "Es un lenguaje sencillo de leer y escribir debido a su alta similitud con el lenguaje humano.  ",
-            c: "Lenguaje poco integrado en el entorno laboral.",
+            a: "Es un lenguaje difícil de escribir y comprender. <br>",
+            b: "Es un lenguaje sencillo de leer y escribir debido a su alta similitud con el lenguaje humano. <br>",
+            c: "Lenguaje poco integrado en el entorno laboral. <br>",
         },
         respuestaCorrecta: "b",
     },
@@ -102,7 +101,7 @@ function mostrarTest() {
     preguntas.forEach((preguntaActual, numeroDePregunta) => {
         const respuestas = [];
 
-        for (let letraRespuesta in preguntaActual.respuestas) {
+        for (letraRespuesta in preguntaActual.respuestas) {
             respuestas.push(
                 `<label>
                   <input type="radio" name="${numeroDePregunta}" value="${letraRespuesta}" />
